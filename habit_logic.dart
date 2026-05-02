47f83b7d1caac82e62981f9dfc4b285302016232
Raihan Athallah <rathallah03@gmail.com>
2026-05-02 10:57:12 +0700

    Implement Habit and Todo management classes
    Added Habit and Todo classes with functionality to manage habits and todos.

--- habit_logic.dart
+++ habit_logic.dart
@@ -1,2 +1,43 @@
-printf("tes");
-return 0;
+class Habit {
+  String name;
+  bool isDone;
+
+  Habit(this.name, {this.isDone = false});
+}
+
+class Todo {
+  String title;
+  bool isDone;
+
+  Todo(this.title, {this.isDone = false});
+}
+
+List<Habit> habitList = [];
+List<Todo> todoList = [];
+int air = 0;
+
+void tambahHabit(String nama) {
+  habitList.add(Habit(nama));
+}
+
+void checkHabit(int index) {
+  if (!habitList[index].isDone) {
+    habitList[index].isDone = true;
+    tambahAir();
+  }
+}
+
+void tambahTodo(String title) {
+  todoList.add(Todo(title));
+}
+
+void checkTodo(int index) {
+  if (!todoList[index].isDone) {
+    todoList[index].isDone = true;
+    tambahAir();
+  }
+}
+
+void tambahAir() {
+  air++;
+}
